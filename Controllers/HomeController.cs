89a8f4827b38a32243bb1dfbfcc0f7c17599b999
Filip Wspaniały <Filip Wspaniay>
2017-01-06 12:49:06 +0100

    Tworzenie widoku strony głównej

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,21 @@ namespace Projekt.Controllers
     {
         public ActionResult Index()
         {
+            /* TODO: pobieranie nowości z bazy danych
+             * - najnowszy zespół
+             * - najnowsza piosenka
+             * - najnowszy artysta
+             * */
+            ViewBag.NewBandImg = "http://www.sbs.com.au/popasia/sites/sbs.com.au.popasia/files/black-pink.jpg";
+            ViewBag.NewBandHeader = "Black Pink";
+            ViewBag.NewBandType = "Girls band";
+            ViewBag.NewBandDate = "01.06.2016";
+            ViewBag.NewBandSex = ViewBag.NewBandType == "Girls band" ? "członkiń" : "członków";
+            ViewBag.NewBandMembers = "4";
+
+            ViewBag.NewSondHeader = "Nowa piosenka";
+            ViewBag.NewArtistHeader = "Nowy artysta";
+
             return View();
         }
 
