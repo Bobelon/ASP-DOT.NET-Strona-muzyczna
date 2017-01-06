5dba86307337de3a92026cdda55a961b9f107491
Filip Wspaniały <Filip Wspaniay>
2017-01-06 19:34:24 +0100

    Zrobiona przykładowa strona główna

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,13 +17,26 @@ namespace Projekt.Controllers
              * */
             ViewBag.NewBandImg = "http://www.sbs.com.au/popasia/sites/sbs.com.au.popasia/files/black-pink.jpg";
             ViewBag.NewBandHeader = "Black Pink";
-            ViewBag.NewBandType = "Girls band";
-            ViewBag.NewBandDate = "01.06.2016";
+            ViewBag.NewBandType = "Girlsband";
+            ViewBag.NewBandDate = "08.08.2016";
             ViewBag.NewBandSex = ViewBag.NewBandType == "Girls band" ? "członkiń" : "członków";
             ViewBag.NewBandMembers = "4";
 
-            ViewBag.NewSondHeader = "Nowa piosenka";
-            ViewBag.NewArtistHeader = "Nowy artysta";
+            ViewBag.NewSongImg = "http://scontent.cdninstagram.com/t51.2885-15/s750x750/sh0.08/e35/12784053_1578386632482075_5357907_n.jpg?ig_cache_key=MTIwMTE2MzU0MDgyNjM5MjgyMw%3D%3D.2";
+            ViewBag.NewSongArtist = "4minute";
+            ViewBag.NewSongName = "Hate";
+            ViewBag.NewSongDate = "31.01.2016";
+
+            ViewBag.NewArtistImg = "https://1.soompi.io/wp-content/uploads/2014/09/B-A-P-Zelo-bap-best-absolute-perfect-33202402-800-1200-e1432334377803.jpg";
+            ViewBag.NewArtistHeader = "Zelo";
+            ViewBag.NewArtistType = "Raper w grupie B.A.P";
+            ViewBag.NewArtistName = "Choi Jun-hong (최준홍)";
+            int day = 15;
+            int month = 11;
+            int year = 1996;
+            ViewBag.NewArtistDate = "15.11." + year;
+            ViewBag.NewArtistAge = DateTime.Now.Year - year;
+            
 
             return View();
         }
---
+++ Models/Band.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Projekt.Models
+{
+    public class Band
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Debiut { get; set; }
+        public DateTime End { get; set; }
+        //public List<Artist> ActualArtists { get; set; }
+        // public List<Artist> ExArtists { get; set; }
+        public string Image { get; set; }
+        //public List<Album> Albums { get; set; }
+        // public List<Song> Songs { get; set; }
+    }
+
+    public class BandDbContext : DbContext
+    {
+        public DbSet<Band> Bands { get; set; }
+    }
+}
